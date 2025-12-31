2aa7d3839980abf9ac4fbb6beb668c5fa85e4808
Stepan Primshits <xseljvm@gmail.com>
2025-12-31 19:21:59 +0300

    otstupi

--- components/Header.tsx
+++ components/Header.tsx
@@ -47,7 +47,7 @@ export function Header({
             Buzzcut Season <span className="text-pink-400">Marketplace</span>
           </div>
           <div className="text-sm text-[var(--muted)] mt-1">
-            Marketplace for the chosen few, by ancient right
+            Marketplace for the chosen, by ancient right
           </div>
         </div>
 
